b00b7b33d7fb460969eb127e89a14dba05033f0d
S0nic014 <47799401+S0nic014@users.noreply.github.com>
2022-03-26 16:20:44 +0000

    Update dependancies versions

--- build_windows.sh
+++ build_windows.sh
@@ -1,11 +1,17 @@
  #!/bin/bash  
 APP_NAME="reference_player"
 MAIN_FILE_PATH=reference_player/main.py
-SITE_PACKAGES_PATH=.env/Lib/site-packages
+SITE_PACKAGES_PATH=.venv/Lib/site-packages
 RES_FOLDER="./res;./res"
 DEFAULT_CONFIG="default_config.json;."
 ICON_PATH="./res/images/player_icon.ico"
 
+
+# Cleanup
+echo "Cleaning up build files"
+rm -rf build dist reference_player.spec
+
+echo "Building with pyinstaller"
 py -3 -m PyInstaller --onedir --noconsole \
 --icon $ICON_PATH \
 --paths $SITE_PACKAGES_PATH \
