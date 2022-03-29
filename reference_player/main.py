0a0ad2f414748e7a361a6c30d371be9f921b4d1a
Dmitrii Shevchenko <47799401+S0nic014@users.noreply.github.com>
2022-03-29 18:40:17 +0100

    Enable layer enviroment variable for MacOS

--- reference_player/main.py
+++ reference_player/main.py
@@ -1,8 +1,10 @@
+import os
 import sys
 from reference_player.player_application import QReferencePlayerApplication
 
 
 if __name__ == "__main__":
+    os.environ["QT_MAC_WANTS_LAYER"] = "1"
     app = QReferencePlayerApplication(sys.argv)
     ret_code = app.exec_()
     app.config.save()
