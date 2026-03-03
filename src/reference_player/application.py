e3b94343e6e7f77f02f2319070375c9878f4441c
sowwic <47799401+sowwic@users.noreply.github.com>
2026-03-03 01:00:44 +0000

    Set application name and version

--- src/reference_player/application.py
+++ src/reference_player/application.py
@@ -2,6 +2,8 @@ import pathlib
 from PySide6 import QtWidgets
 from PySide6 import QtQml
 
+from reference_player.version import __version__
+
 
 APPLICATION_QML_SOURCE = (
     pathlib.Path(__file__).absolute().resolve().parent / "qml" / "Application.qml"
@@ -11,6 +13,10 @@ APPLICATION_QML_SOURCE = (
 class ReferencePlayerApplication(QtWidgets.QApplication):
     def __init__(self, argv):
         super().__init__(argv)
+        self.setApplicationVersion(__version__)
+        self.setApplicationName("reference-player")
+        self.setApplicationDisplayName(f"Reference Player v{__version__}")
+
         self.engine = QtQml.QQmlApplicationEngine()
         self.engine.quit.connect(self.quit)
         self.engine.load(APPLICATION_QML_SOURCE)
